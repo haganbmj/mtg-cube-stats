3b024c71697c18aba5e5b530ee62dee263b7d4e2
Brendan Hagan <bhaganksu@gmail.com>
2025-12-04 17:17:51 -0500

    misc: add another peasant cube (#122)

--- preload-cube-data.mjs
+++ preload-cube-data.mjs
@@ -173,6 +173,7 @@ const batches = [
             '5d72c6cad13edd6540aff5f3', // cr4cksh07 - Mad Peasant
             '5d498d7797ca265764f113bf', // DammitJoel - Peasant Synergy Cube
             '5f76cacbdebf310362b90289', // ZeldaExplorer - ZeldaExplorer's Peasant Cube
+            '5e4fec00b2fc055a2a149986', // WasherDryer - Peasant Cube
         ],
     },
     {
