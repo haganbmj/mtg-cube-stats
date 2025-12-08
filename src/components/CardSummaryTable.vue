dab36c9d37cb11427e346cf3a58134be6e466e3e
Brendan Hagan <bhaganksu@gmail.com>
2025-12-07 23:29:49 -0500

    feat: add tooltip for cube owner to similar + cards tables (#134)

--- src/components/CardSummaryTable.vue
+++ src/components/CardSummaryTable.vue
@@ -74,7 +74,9 @@
                                     <div v-if="cube.included">
                                         <el-row direction="horizontal">
                                             <el-col :span="16">
-                                                <el-link :href="`https://cubecobra.com/cube/overview/${cube.id}`" target="_blank">{{ cube.name }}</el-link>
+                                                <el-tooltip :content="`Owner: ${cube.owner}`" placement="top" :hide-after="50">
+                                                    <el-link :href="`https://cubecobra.com/cube/overview/${cube.id}`" target="_blank">{{ cube.name }}</el-link>
+                                                </el-tooltip>
                                             </el-col>
                                             <el-col :span="8">
                                                 <el-text tag="i">({{ cube.size }} Cards)</el-text>
@@ -89,7 +91,9 @@
                                     <div v-if="!cube.included">
                                         <el-row direction="horizontal">
                                             <el-col :span="16">
-                                                <el-link :href="`https://cubecobra.com/cube/overview/${cube.id}`" target="_blank">{{ cube.name }}</el-link>
+                                                <el-tooltip :content="`Owner: ${cube.owner}`" placement="top" :hide-after="50">
+                                                    <el-link :href="`https://cubecobra.com/cube/overview/${cube.id}`" target="_blank">{{ cube.name }}</el-link>
+                                                </el-tooltip>
                                             </el-col>
                                             <el-col :span="8">
                                                 <el-text tag="i">({{ cube.size }} Cards)</el-text>
