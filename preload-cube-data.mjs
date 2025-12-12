9365f88d974ac904580df42909b139038353a123
Brendan Hagan <bhaganksu@gmail.com>
2025-12-12 16:36:47 -0500

    feat: add Vertex Philly 2026

--- preload-cube-data.mjs
+++ preload-cube-data.mjs
@@ -188,6 +188,30 @@ const batches = [
         ],
     },
     {
+        name: 'vertex-philly-2026',
+        staleThreshold: Date.now() - (1000 * 60 * 60 * 24 * 1), // 1 day
+        shardCount: 3,
+        cubes: [
+            '05426587-9751-4292-b1f8-e85c37e74223', // monicamillions - Girls' Night
+            '0718b9a8-7580-47da-bd5e-3b3a1701fb3a', // haganbmj - The Kuleshov Cube
+            '2f166be5-e08e-4223-8883-4782471f58b4', // Kerby - Omnipresent Imposters
+            'e466a21c-0a3a-433f-8196-7fcdb844f430', // jms772 - Jason's Lightweight Cube
+            '272013bb-a5a2-4e3e-a666-23bf90b26fb8', // takato3109 - Science Cube
+            '62eee89b3106370bdfeffdb2', // vedalkensamurai - Standard Metagame Cube '23-'26
+            '549eb6df-e390-4eb3-895d-f1e1cac77f7b', // frauwacholder - B.O.N.G. cube
+            '0bbec4a4-a4e1-4474-8832-17deabedf622', // Sulla20XX - The Whetting Stone
+            '61d31f05be8c31103b575e0a', // TheOinkinator - Bolt Cube
+            'b67c9d5e-5cf5-46f6-bb3d-58a014df6a1a', // dinrovahorror - Companion Cube
+            '61454789685c83106293be3c', // Deinonychus - Bodleian Cube
+            'cb3f23e0-5e6e-44e3-bc40-45b1132942ad', // griffv - Hall of Legends
+            '379dfcb9-3128-422d-adf0-2b8778176c03', // Games - Core Set 19XX
+            'dafb7624-8329-4ac7-af2f-7ad80f94b038', // frogirl - The Wetlands
+            '8e138c2c-0011-4cd0-aead-ac9c50f53939', // AndyOnline - The Space Between
+            'e353934d-cfe8-4eb2-90f5-6e6688b86ebb', // monadic - The Trading Post Cube
+            '9612ef94-a40f-446d-9244-a19a94dbcbcb', // SoftMaurice - Trenchfight
+        ],
+    },
+    {
         name: 'cubecon2025',
         staleThreshold: undefined,
         shardCount: undefined,
