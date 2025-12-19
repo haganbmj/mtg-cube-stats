56aea91ff1b4d2d06710ba92d4bd71b9dcf2e75f
Brendan Hagan <bhaganksu@gmail.com>
2025-12-19 11:41:17 -0500

    misc: add sharkman peasant

--- preload-cube-data.mjs
+++ preload-cube-data.mjs
@@ -197,6 +197,7 @@ const batches = [
             '5ec5c9197868862b1f42c0d1', // Double - The Silver Lining
             '60eb90e47bdeb510201cb11e', // RyanOverdrive - Peasant Twobert
             '557847ff-ac94-428e-89ab-4d8991c45266', // Oophies - Peasant Desert Cube
+            'fab2344b-bfaf-4140-96a7-42ad314e5b44', // Sharkman1231 - Fetch Peasant
         ],
     },
     {
