015565296f203e56df681ea043ac05dc3ac0cbfa
Brendan Hagan <bhaganksu@gmail.com>
2026-04-15 16:44:23 -0400

    misc: more peasant

--- preload-cube-data.ts
+++ preload-cube-data.ts
@@ -133,6 +133,7 @@ const batches: Batch[] = [
             '3763af72-6ad9-4f01-9264-318c57df3aa7', // Uteqoute - Uteqoute's 360 Peasant C/Ube [No UB]
             '375a3eb7-33bc-45e9-821e-e699cf196ace', // PepperMyr - PepperMyr's 360 Peasant Cube
             '5f1c4ebcbb7e6b105ca59780', // rancoredmalone - Peasant Snow CUbe
+            '315272ac-9bc6-45ee-82d4-2e2279b034fe', // SultanYakub - Resource Cube (Open Beta)
         ],
     },
     {
