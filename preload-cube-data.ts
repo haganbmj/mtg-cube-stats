29bc9fb6358282131d5ff96db953ffd95a22711a
Brendan Hagan <bhaganksu@gmail.com>
2026-05-04 23:20:30 -0400

    misc: minty peasant

--- preload-cube-data.ts
+++ preload-cube-data.ts
@@ -137,6 +137,7 @@ const batches: Batch[] = [
             '315272ac-9bc6-45ee-82d4-2e2279b034fe', // SultanYakub - Resource Cube (Open Beta)
             '8cf14ff3-b9fa-486e-9faf-ce37c1a8ccb1', // emielboven - Emiel's Peasant Cube
             '5dc657d5930245513fdff287', // campervanbeethoven - Uncommon Cube
+            'cccf5f35-7800-4135-a52d-1678d9dec978', // Minty - Minty 360 Peasant Cube
         ],
     },
     {
