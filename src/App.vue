eece138044cb0e1c2f55362c5380b0b45b0dbfe8
Brendan Hagan <bhaganksu@gmail.com>
2026-05-15 22:24:49 -0400

    misc: switch to social media images (#389)

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,13 @@
                         <el-breadcrumb-item class="header-page-title">Cube Comparison</el-breadcrumb-item>
                     </el-breadcrumb>
                     <div class="header-links">
-                        <a href="https://bsky.app/profile/griselbrand.com" target="_blank">Bluesky</a>
+                        <a href="https://bsky.app/profile/griselbrand.com" target="_blank" title="Bluesky">
+                            <svg fill="none" viewBox="0 0 64 57" width="22" style="height: auto;"><path fill="currentColor" d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"/></svg>
+                        </a>
                         <el-divider direction="vertical" />
-                        <a href="https://github.com/haganbmj/mtg-cube-stats" target="_blank">Github</a>
+                        <a href="https://github.com/haganbmj/mtg-cube-stats" target="_blank" title="GitHub">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+                        </a>
                     </div>
                 </div>
             </el-header>
@@ -466,8 +470,8 @@ html.dark .el-dropdown__popper {
 }
 
 .el-header {
-    padding-top: 1em;
-    padding-bottom: 1em;
+    padding-top: 0.8em;
+    padding-bottom: 0.8em;
     height: unset;
 
     background: var(--el-fill-color-lighter);
@@ -499,6 +503,10 @@ html.dark .el-dropdown__popper {
     align-items: center;
     white-space: nowrap;
     flex-shrink: 0;
+
+    svg {
+        display: block;
+    }
 }
 
 @media (max-width: 480px) {
