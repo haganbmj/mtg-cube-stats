06ec027b78afedcb858678f649688e0a41f3acc6
Brendan Hagan <bhaganksu@gmail.com>
2026-05-29 15:24:17 -0400

    misc: more sort changes

--- src/util/SortConfig.ts
+++ src/util/SortConfig.ts
@@ -21,8 +21,8 @@ export const cardSortProperties: SortProperty[] = [
     { prop: 'minPriceUsd', label: 'Price (USD)', autoDirection: 'descending' },
     { prop: 'minPriceTix', label: 'Price (Tix)', autoDirection: 'descending' },
     { prop: 'oracleTextWordCountMinusParen', label: 'Word Count', autoDirection: 'descending' },
-    { prop: 'power', label: 'Power', autoDirection: 'ascending' },
-    { prop: 'toughness', label: 'Toughness', autoDirection: 'ascending' },
+    { prop: 'power', label: 'Power', autoDirection: 'descending' },
+    { prop: 'toughness', label: 'Toughness', autoDirection: 'descending' },
 ];
 
 export const overviewSortProperties: SortProperty[] = [
@@ -34,7 +34,7 @@ export const overviewSortProperties: SortProperty[] = [
     { prop: 'lastModified', label: 'Modified Date', autoDirection: 'descending' },
     { prop: 'followerCount', label: 'Followers', autoDirection: 'descending' },
     { prop: 'stats.averageNonLandCmc', label: 'Avg Mana Value', autoDirection: 'ascending' },
-    { prop: 'stats.averageWordCount', label: 'Avg Word Count', autoDirection: 'ascending' },
+    { prop: 'stats.averageWordCount', label: 'Avg Word Count', autoDirection: 'descending' },
     { prop: 'stats.uniqueNonEvergreenKeywords', label: 'Non-Evergreen Keywords', autoDirection: 'descending' },
     { prop: 'stats.cardCounts.makesTokens', label: 'Makes Tokens', autoDirection: 'descending' },
     { prop: 'stats.medianReleaseYear', label: 'Median Release Year', autoDirection: 'descending' },
