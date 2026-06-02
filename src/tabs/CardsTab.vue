e6e6c76d57d9378fe80c913e3c0b127b5357b071
Brendan Hagan <bhaganksu@gmail.com>
2026-06-02 16:58:05 -0400

    fix: update cards tab to indicate cubes are loading (#444)

--- src/tabs/CardsTab.vue
+++ src/tabs/CardsTab.vue
@@ -1,13 +1,27 @@
 <template>
     <div style="width: 100%;">
-        <CardSummaryTable :loaded-cubes="loadedCubes" :similarity-matrix="similarityMatrix" :overview-table-data="overviewTableData" />
+        <!-- Loading state -->
+        <div v-if="props.loadingProgress?.active || !scryfallReady" style="padding: 40px; text-align: center;">
+            <el-text v-if="!scryfallReady" type="info" style="display: block; margin-bottom: 12px;">Loading card data&hellip;</el-text>
+            <template v-if="props.loadingProgress?.active">
+                <el-text type="info" style="display: block; margin-bottom: 12px;">Loading cubes...</el-text>
+                <el-progress
+                    :percentage="props.loadingProgress.total > 0 ? Math.round((props.loadingProgress.loaded / props.loadingProgress.total) * 100) : 0"
+                    :format="() => `${props.loadingProgress!.loaded} / ${props.loadingProgress!.total}`"
+                />
+            </template>
+        </div>
+
+        <!-- Normal content -->
+        <CardSummaryTable v-else :loaded-cubes="loadedCubes" :similarity-matrix="similarityMatrix" :overview-table-data="overviewTableData" />
     </div>
 </template>
 
 <script setup lang="ts">
 import CardSummaryTable from '../components/CardSummaryTable.vue';
+import { scryfallReady } from '../util/CubeFunctions';
 
-defineProps({
+const props = defineProps({
     loadedCubes: {
         type: Object,
         required: true,
@@ -19,6 +33,10 @@ defineProps({
     overviewTableData: {
         type: Array,
         required: true,
+    },
+    loadingProgress: {
+        type: Object as () => { active: boolean; loaded: number; total: number } | null,
+        default: null,
     },
 });
 </script>
