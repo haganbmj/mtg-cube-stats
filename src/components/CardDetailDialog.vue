f55e13e0073a652be64e4e917593f207a96aee6d
Brendan Hagan <bhaganksu@gmail.com>
2026-07-01 22:56:52 -0400

    fix: ensure lazy loading on views with multiple images

--- src/components/CardDetailDialog.vue
+++ src/components/CardDetailDialog.vue
@@ -33,7 +33,7 @@
                             fit="contain"
                             :alt="showFront ? activeCard.name : (activeCard.name ?? '') + ' (back)'"
                             :class="'card-image ' + activeCard.setCode?.toLowerCase()"
-                            style="width: 100%;"
+                            style="width: 100%; aspect-ratio: 63 / 88;"
                         />
                         <div v-if="activeCard.urlBack" style="margin-top: 8px;">
                             <el-button size="small" @click="showFront = !showFront">
