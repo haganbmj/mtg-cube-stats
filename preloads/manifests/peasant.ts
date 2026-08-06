b88a1c9e79032a277a3dbf50fc97ac81aa99ade3
Brendan Hagan <bhaganksu@gmail.com>
2026-08-06 18:02:12 -0400

    misc: more peasant cubes

--- preloads/manifests/peasant.ts
+++ preloads/manifests/peasant.ts
@@ -114,6 +114,7 @@ const manifest: Manifest = {
         '637b9e8cd7d17f14ab27f4c9', // RoroLeZozo - Synergy Peasant Cube
         '5dbcbd2a44e9cb7a6c8097cf', // ahhabee - C/U be
         '5e539a45e74a617cdd462f32', // gsethi - Gsethi 360 Peasant Cube
+        '9bd7c4dc-408d-4ebb-b08a-039528a4a50e', // CloudedBirb - Peasant Vintage PowerMax!
     ],
 };
 
