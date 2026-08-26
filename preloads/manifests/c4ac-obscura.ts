c7d7e3ae3e6984f7810673ceb36f247465908a6b
Brendan Hagan <bhaganksu@gmail.com>
2026-08-25 21:10:47 -0400

    misc: add c4ac obscura (#583)

---
+++ preloads/manifests/c4ac-obscura.ts
@@ -0,0 +1,29 @@
+import type { Manifest } from './types';
+
+const manifest: Manifest = {
+    name: 'c4ac-obscura',
+    label: 'Cube for a Cause: Obscura',
+    description: 'Brooklyn, New York - August 22-23, 2026',
+    fetch: { staleThreshold: '7d', shardCount: 2 },
+    cubes: [
+        'ac957ac6-4fb3-44f3-bbca-e8c9efd25656',
+        'ce5a78ae-84c7-4db9-9230-c7c943b5d97b',
+        'bae9c4ab-545e-496c-900b-2b65a95e0a33',
+        '5a81d61c-db43-4ed3-b0f9-59f7a18ba3d6',
+        '0e1acec1-662a-4607-b1e3-a1f0ffa798b8',
+        '37ee652a-447e-40c1-9546-5602517b1ab7',
+        'd677a4ba-60c8-4117-adc9-7d76e10a2c34',
+        '7df87ddf-40f5-4c5b-8de2-e1ce223bddf0',
+        '7bfffb69-9667-462b-8412-ce55285fbd3f',
+        'cd250ad9-ce97-4d14-9400-b7b95ca6e8c4',
+        'c5817b7b-78c7-4115-9533-d7815a237877',
+        'bc8ed74c-2699-4137-b028-4b42c4754c81',
+        '6b68ac30-dc0d-4936-8e04-8f62a436a48b',
+        'f6fb856a-8550-43b6-8bf9-cb9f0730c9c1',
+        'b8f4d339-5b0c-4b84-a622-7aaa0b518080',
+        'a55dc0cf-2390-45a1-ba37-b19b9f422f7d',
+        'c2264d83-7481-46cf-944b-e2798583d954',
+    ],
+};
+
+export default manifest;
